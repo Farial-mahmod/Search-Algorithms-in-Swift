fac8327533b014645e6a0923852615152d8712be
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2023-07-10 12:26:00 +0600

    adding the linearSearch.swift file

---
+++ Linear Search/linearSearch.swift
@@ -0,0 +1,19 @@
+// Linear Search implementation in Swift (version 5.8.1)
+
+import Foundation
+
+func linearSearch(_ elements: [Int], _ searchElement: Int) {
+
+// the elements are to be sorted to proceed
+
+// looping through the elements array
+for i in elements {
+// if the element to be searched is found, printing it with its position
+if(searchElement==elements[i]){
+  print("Found \(searchElement) at index \(i)")
+  }
+ }
+}
+
+// calling the function to execute
+print(linearSearch([1,2,3,4,5,6,7,8],2))
